01216d7d5856b7a5bf21a5b5e3a333b284b265fb
Luis Parrado <luisprmat@gmail.com>
2024-02-29 23:13:10 -0500

    Remove hard-coded messages

--- app/Livewire/Messages.php
+++ app/Livewire/Messages.php
@@ -2,32 +2,28 @@
 
 namespace App\Livewire;
 
-use Illuminate\Support\Carbon;
+use App\Models\Message;
+use Illuminate\Database\Eloquent\Builder;
 use Livewire\Component;
 
 class Messages extends Component
 {
-    public array $messages = [];
-
-    public function mount()
-    {
-        // Hard-coded messages
-        $this->messages = [
-            [
-                'time' => Carbon::now()->format('g:i:sa'),
-                'message' => 'Hola a todos',
-                'private' => false,
-            ],
-            [
-                'time' => Carbon::now()->format('g:i:sa'),
-                'message' => 'Hola usuario privado',
-                'private' => true,
-            ],
-        ];
-    }
+    public ?int $userId = null;
 
     public function render()
     {
-        return view('livewire.messages');
+        if ($authUser = auth()->user()) {
+            $this->userId = $authUser->id;
+        }
+
+        $messages = Message::where('private', false)
+            ->orWhere(function (Builder $query) {
+                $query->when($this->userId, function (Builder $q) {
+                    $q->where('user_id', $this->userId);
+                });
+            })
+            ->get();
+
+        return view('livewire.messages', compact('messages'));
     }
 }
